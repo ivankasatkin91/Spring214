c50af0a3d3a2e64e4e7e2028159044508adeda03
ivankasatkin91 <ivankasatkin91@gmail.com>
2022-10-26 19:50:09 +0300

    release 1.01: minor string errors fix

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -10,6 +10,6 @@ public class Island2 {
 
     @Override
     public String toString() {
-        return ", на острове дерево 2" + wood.toString();
+        return ", на острове дерево " + wood.toString();
     }
 }
